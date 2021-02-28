7947bc09c7f82eeea5ed0e049fd8172769acf99d
LevBernstein <lev2000@me.com>
2021-02-28 13:28:11 -0500

    addRange, lookup

--- twothree.java
+++ twothree.java
@@ -29,7 +29,7 @@ class TwoThreeTree {
     TwoThreeTree() {
         root = null;
         height = -1;
-        // empty tree has a height of -1; tree with just root has a height of 0, as the root is 0 away from the leaves
+        // empty tree has a height of -1; tree with just root has a height of 0, as the root is 0 away from the leaf (itself).
     }
 }
 
@@ -57,7 +57,7 @@ public class twothree {
         String planetName;
         String secondPlanet;
         String[] temp;
-        
+        TwoThreeTree tree = new TwoThreeTree();
         queries = scan.nextInt();
         scan.nextLine();
         
@@ -67,34 +67,65 @@ public class twothree {
             planetName = temp[1];
             if (type == 1) {
                 entranceFee = Integer.parseInt(temp[2]);
+                insert(planetName, entranceFee, tree);
             }
             else if (type == 2) {
                 secondPlanet = temp[2];
                 entranceFee = Integer.parseInt(temp[3]);
+                addRange(tree.root, planetName, secondPlanet, tree.height, "", entranceFee;
             }
             else if (type == 3) {
-                
+                entranceFee = lookup(planetName, tree.root, 0);
+                output.write(String.valueOf(entranceFee) + "\n");
             }
         }
         
         output.flush(); //flush System.out at the end of main
     }
+            
+    static int lookup(String target, Node p, int total) {
+        if (p instanceof LeafNode) {
+            if (target.compareTo(p.guide) == 0)
+                return total + p.value;
+            return -1;
+            }
+        
+        InternalNode internal = (InternalNode) p;
+        if (target.compareTo(internal.child0.guide) <= 0)
+            return lookup(target, internal.child0, total + internal.value);
+        if (target.compareTo(internal.child0.guide) > 0 && target.compareTo(internal.child1.guide) <= 0)
+            return lookup(target, internal.child1, total + internal.value);
+        if (internal.child2 != null && target.compareTo(internal.child1.guide) > 0)
+            return lookup(target, internal.child2, total + internal.value);
+        return -1;
+    }
     
-    static void addRange(Node p, String x, String y, int h, String lo, int delta, BufferedWriter output) throws Exception {
-        if (h == 0) && (p.guide.compareTo(x) >= 0 && p.guide.compareTo(y) <= 0) {
+    static void addRange(Node p, String x, String y, int h, String lo, int delta) {
+        if (h == 0) {
+            if (p.guide.compareTo(x) >= 0 && p.guide.compareTo(y) <= 0) {
+                p.value += delta;
+            }
+            return;
+        }
+        if (y.compareTo(lo) <= 0)
+            return;
+        String hi = p.guide;
+        if (hi.compareTo(x) < 0)
+            return;
+        if (x.compareTo(lo) <= 0 && hi.compareTo(y) <= 0) {
             p.value += delta;
             return;
         }
-        
-        
+        InternalNode internal = (InternalNode) p; // must cast to an internal node in order to access child0, child1, child2
+        addRange(internal.child0, x, y, h - 1, lo, delta);
+        addRange(internal.child1, x, y, h - 1, internal.child0.guide, delta);
+        if (internal.child2 != null)
+            addRange(internal.child2, x, y, h - 1, internal.child1.guide, delta);
     }
             
     static void insert(String key, int value, TwoThreeTree tree) {
         // insert a key value pair into tree (overwrite existing value if key is already present)
-        
         int h = tree.height;
-        
-        
         if (h == -1) { //for an empty tree:
             LeafNode newLeaf = new LeafNode();
             newLeaf.guide = key;
@@ -102,13 +133,10 @@ public class twothree {
             tree.root = newLeaf; 
             tree.height = 0;
         }
-        
         else {
             WorkSpace ws = doInsert(key, value, tree.root, h);
-            
             if (ws != null && ws.newNode != null) {
                 // create a new root
-                
                 InternalNode newRoot = new InternalNode();
                 if (ws.offset == 0) {
                     newRoot.child0 = ws.newNode; 
@@ -118,7 +146,6 @@ public class twothree {
                     newRoot.child0 = tree.root; 
                     newRoot.child1 = ws.newNode;
                 }
-                
                 resetGuide(newRoot);
                 tree.root = newRoot;
                 tree.height = h+1;
@@ -128,38 +155,38 @@ public class twothree {
                 
     static WorkSpace doInsert(String key, int value, Node p, int h) {
         // auxiliary recursive routine for insert
-        
         if (h == 0) {
             // we're at the leaf level, so compare and either update value or insert new leaf
-            
             LeafNode leaf = (LeafNode) p; //downcast
             int cmp = key.compareTo(leaf.guide);
-            
             if (cmp == 0) {
                 leaf.value = value; 
                 return null;
             }
-            
             // create new leaf node and insert into tree
             LeafNode newLeaf = new LeafNode();
             newLeaf.guide = key; 
             newLeaf.value = value;
-            
             int offset = (cmp < 0) ? 0 : 1;
             // offset == 0 => newLeaf inserted as left sibling
             // offset == 1 => newLeaf inserted as right sibling
-            
             WorkSpace ws = new WorkSpace();
             ws.newNode = newLeaf;
             ws.offset = offset;
             ws.scratch = new Node[4];
-            
             return ws;
         }
         else {
             InternalNode q = (InternalNode) p; // downcast
             int pos;
             WorkSpace ws;
+            
+            //new logic for shifted value, zeroing out:
+            q.child0.value += q.value;
+            q.child1.value += q.value;
+            if (q.child2 != null)
+                q.child2.value += q.value;
+            q.value = 0;
             
             if (key.compareTo(q.child0.guide) <= 0) {
                 pos = 0; 
@@ -173,7 +200,6 @@ public class twothree {
                 pos = 2; 
                 ws = doInsert(key, value, q.child2, h-1);
             }
-            
             if (ws != null) {
                 if (ws.newNode != null) {
                     // make ws.newNode child # pos + ws.offset of q
@@ -195,14 +221,12 @@ public class twothree {
                     ws.guideChanged = resetGuide(q);
                 }
             }
-                    
             return ws;
         }
     }
                                     
     static int copyOutChildren(InternalNode q, Node[] x) {
         // copy children of q into x, and return # of children
-        
         int sz = 2;
         x[0] = q.child0; x[1] = q.child1;
         if (q.child2 != null) {
@@ -215,37 +239,30 @@ public class twothree {
     static void insertNode(Node[] x, Node p, int sz, int pos) {
         // insert p in x[0..sz) at position pos,
         // moving existing extries to the right
-        
         for (int i = sz; i > pos; i--)
             x[i] = x[i-1];
-        
         x[pos] = p;
     }
     
     static boolean resetGuide(InternalNode q) {
         // reset q.guide, and return true if it changes.
-        
         String oldGuide = q.guide;
         if (q.child2 != null)
             q.guide = q.child2.guide;
         else
             q.guide = q.child1.guide;
-        
         return q.guide != oldGuide;
     }
     
     static boolean resetChildren(InternalNode q, Node[] x, int pos, int sz) {
         // reset q's children to x[pos..pos+sz), where sz is 2 or 3.
         // also resets guide, and returns the result of that
-        
         q.child0 = x[pos]; 
         q.child1 = x[pos+1];
-        
         if (sz == 3) 
             q.child2 = x[pos+2];
         else
             q.child2 = null;
-        
         return resetGuide(q);
     }
     
